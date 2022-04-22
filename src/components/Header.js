dce2676938dc98a5b12f2a147b4a32dc582054a6
samuel santos <samaraujo28@gmail.com>
2022-04-21 21:14:10 -0300

    [feat] add Links on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -33,6 +34,26 @@ class Header extends Component {
         <div>
           <span data-testid="header-user-name">{ userName }</span>
         </div>
+        <nav data-testid="header-component">
+          <Link
+            to="/search"
+            data-testid="link-to-search"
+          >
+            Pesquisa
+          </Link>
+          <Link
+            to="/favorites"
+            data-testid="link-to-favorites"
+          >
+            Pesquisa
+          </Link>
+          <Link
+            to="/profile"
+            data-testid="link-to-profile"
+          >
+            Pesquisa
+          </Link>
+        </nav>
       </div>
     );
 
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import '../index.css';
 
 class Loading extends Component {
   render() {
     return (
-      <div>
+      <div className="loading">
         <p>Carregando...</p>
       </div>
     );
